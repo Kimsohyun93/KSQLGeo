0f450e08a161cb1dc328989c10b3143e736de08b
Kimsohyun93 <shk980903@naver.com>
2022-08-26 15:40:56 +0900

    test value

---
+++ src/main/java/com/github/wlaforest/ksql/udf/GeoIntersectedCircle.java
@@ -0,0 +1,25 @@
+package com.github.wlaforest.ksql.udf;
+
+
+import com.github.wlaforest.geo.GeometryParseException;
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
+
+@UdfDescription(
+        name = "intersected_circle",
+        description = "UDF function to test for geometry intersection in euclidean space. geometry encoded in " +
+                "WKT or GeoJSON. null value result in false being returned.",
+        version = "1.3.1",
+        author = "Will LaForest"
+)
+public class GeoIntersectedCircle extends GeometryBase {
+  @Udf(description = "determines if a the two geometries intersect.")
+  public boolean geo_intersected (
+          @UdfParameter(value = "polygon", description = "WKT or GeoJSON Encoded Geometry to check for intersection with polygon") final String polygon)
+          throws GeometryParseException {
+
+    System.out.println(polygon);
+    return true;
+  }
+}
